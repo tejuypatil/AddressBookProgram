547fd40120c45eba36017ce4f56e7cb49833fce6
tejuypatil <tejuypatil@gmail.com>
2022-07-16 13:52:18 +0530

    Added Welcome msg

---
+++ src/com/bridgelabz/address/booksystem/AddressBookSystem.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.address.booksystem;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+
+}
