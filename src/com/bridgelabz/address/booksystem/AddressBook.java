84f7a7c1e1880d549ee4e3b3478934f94040716b
tejuypatil <tejuypatil@gmail.com>
2022-07-19 23:51:49 +0530

    Added  new contact by using UC5

--- src/com/bridgelabz/address/booksystem/AddressBook.java
+++ src/com/bridgelabz/address/booksystem/AddressBook.java
@@ -1,59 +1,30 @@
 package com.bridgelabz.address.booksystem;
 
-import java.util.Scanner;
-
+import java.util.HashMap;
+import java.util.Map;
 public class AddressBook {
-
-    Contact[] contacts=new Contact[100];
-    int index=0;
-    public void add(Contact contact)
+    Map<String,Contact> contactMap=new HashMap<>();
+    public void addContact(Contact contact)
     {
-        contacts[index]=contact;
-        index++;
+        contactMap.put(contact.firstName,contact);
     }
 
     public void print()
     {
-        for (int i = 0; i < index; i++) {
-            contacts[i].viewData();
+        for (Contact contact : contactMap.values()) {
+            contact.viewData();
         }
     }
 
     public void editContact(String contactName)
     {
-        for (int i=0;i<index;i++)
-        {
-            Contact c=contacts[i];
-            if ( c.firstName.equals(contactName)  )
-            {
-                // c is contact to edit
-                c.viewData();
-                c.askDetailsToUser();
-            }
-        }
-
+        Contact contact=contactMap.get(contactName);
+        contact.viewData();
+        contact.askDetailsToUser();
     }
 
     public void deleteContact(String name)
     {
-        int indexToDelete=-1;
-        for (int i=0;i<index;i++)
-        {
-            Contact c=contacts[i];
-            if ( c.firstName.equals(name)  )
-            {
-                indexToDelete=i;
-                break;
-            }
-        }
-
-        if(indexToDelete > -1)
-        {
-            //delete logic
-            for (int i = indexToDelete; i < index ; i++) {
-                contacts[i]=contacts[i+1];
-            }
-            index--;
-        }
+        contactMap.remove(name);
     }
 }
