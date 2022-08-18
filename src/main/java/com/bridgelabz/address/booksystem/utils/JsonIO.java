d13913f3fd33b6fe566d3e612626d4c277608c8b
tejuypatil <tejuypatil@gmail.com>
2022-08-18 10:09:16 +0530

    Added program to Read or Write the Address Book with Persons Contact into a File using Json

---
+++ src/main/java/com/bridgelabz/address/booksystem/utils/JsonIO.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.address.booksystem.utils;
+
+import com.bridgelabz.address.booksystem.Contact;
+
+import java.util.ArrayList;
+
+public class JsonIO implements SaverIo{
+    @Override
+    public void writeData(String fileName, ArrayList<Contact> contacts) {
+
+    }
+
+    @Override
+    public void readData(String fileName, ArrayList<Contact> contacts)  {
+
+    }
+}
